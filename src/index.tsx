d6dd68e0ab2b670f45c821733c77a8b43ac4037a
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2022-03-01 22:25:40 +0530

    add auth0 to the project

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,17 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH0_DOMAIN ?? ""}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID ?? ""}
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
